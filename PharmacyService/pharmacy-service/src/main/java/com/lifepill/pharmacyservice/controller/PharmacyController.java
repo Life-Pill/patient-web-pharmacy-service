7e79f7a062eca8310eb6cce6404b29bf284be6ca
Inupa Vihan <inupavihan9@gmail.com>
2024-03-12 19:36:54 +0530

    Add configurations to the API documentation

--- PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/controller/PharmacyController.java
+++ PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/controller/PharmacyController.java
@@ -16,6 +16,9 @@ import org.springframework.web.bind.annotation.RestController;
 import com.lifepill.pharmacyservice.model.Pharmacy;
 import com.lifepill.pharmacyservice.service.PharmacyService;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+
 @RestController
 @RequestMapping("/pharmacies")
 public class PharmacyController {
@@ -23,42 +26,56 @@ public class PharmacyController {
     private PharmacyService pharmacyService;
 
     // get all pharmacies
+    @Tag(name = "get", description = "GET methods of Pharmacy service API")
+    @Operation(description = "Get all the pharmacies")
     @GetMapping
     public List<Pharmacy> getAllPharmacies() {
         return pharmacyService.getAllPharmacies();
     }
 
     // get all pharmacies by district
+    @Tag(name = "get", description = "GET methods of Pharmacy service API")
+    @Operation(description = "Get all the pharmacies located in a specific district")
     @GetMapping("/district={district}")
     public List<Pharmacy> getPharmacyByDistrict(@PathVariable String district) {
         return pharmacyService.getPharmacyByDistrict(district);
     }
 
     // get all pharmacies by city
+    @Tag(name = "get", description = "GET methods of Pharmacy service API")
+    @Operation(description = "Get all the pharmacies located in a specific city")
     @GetMapping("/city={city}")
     public List<Pharmacy> getPharmacyByCity(@PathVariable String city) {
         return pharmacyService.getPharmacyByCity(city);
     }
 
     // get pharmacy by id
+    @Tag(name = "get", description = "GET methods of Pharmacy service API")
+    @Operation(description = "Get a specific pharmacy")
     @GetMapping("/{id}")
     public Optional<Pharmacy> getPharmacy(@PathVariable Long id) {
         return pharmacyService.getPharmacy(id);
     }
 
     // add new pharmacy
+    @Tag(name = "post", description = "POST methods of Pharmacy service API")
+    @Operation(description = "Create a new pharmacy")
     @PostMapping
     public Pharmacy addPharmacy(@RequestBody Pharmacy newPharmacy) {
         return pharmacyService.addPharmacy(newPharmacy);
     }
 
     // update pharmacy open status
+    @Tag(name = "put", description = "PUT methods of Pharmacy service API")
+    @Operation(description = "Update open status of a pharmacy")
     @PutMapping("/{id}")
     public Pharmacy updatePharmacyOpenStatus(@PathVariable Long id) {
         return pharmacyService.updatePharmacyOpenStatus(id);
     }
 
     // delete pharmacy
+    @Tag(name = "delete", description = "DELETE methods of Pharmacy service API")
+    @Operation(description = "Delete a specific pharmacy")
     @DeleteMapping("/{id}")
     public String deletePharmacy(@PathVariable Long id) {
         pharmacyService.deletePharmacy(id);
@@ -67,6 +84,8 @@ public class PharmacyController {
     }
 
     // delete all pharmacies
+    @Tag(name = "delete", description = "DELETE methods of Pharmacy service API")
+    @Operation(description = "Delete all the pharmacies")
     @DeleteMapping
     public String deleteAllPharmacies() {
         pharmacyService.deleteAllPharmacies();
--- PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/model/Pharmacy.java
+++ PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/model/Pharmacy.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -19,23 +21,44 @@ import lombok.NoArgsConstructor;
 @Table(name = "pharmacy")
 public class Pharmacy {
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "pharm_id")
     private Long pharmacyId;
+
+    @NotNull
+    @Size(min = 1, max = 200)
     @Column(name = "name", nullable = false, length = 200)
     private String pharmacyName;
+
+    @NotNull
+    @Size(min = 1, max = 200)
     @Column(name = "email", nullable = false, unique = true, length = 200)
     private String pharmacyEmail;
+
+    @NotNull
+    @Size(min = 3, max = 50)
     @Column(name = "mobile_number", nullable = false, unique = true, length = 50)
     private String pharmacyMobileNumber;
+
+    @NotNull
+    @Size(min = 1, max = 200)
     @Column(name = "address_street", nullable = false, length = 200)
     private String pharmacyAddressStreet;
+
+    @NotNull
+    @Size(min = 1, max = 200)
     @Column(name = "address_city", nullable = false, length = 200)
     private String pharmacyAddressCity;
+
+    @NotNull
+    @Size(min = 1, max = 100)
     @Column(name = "address_district", nullable = false, length = 100)
     private String pharmacyAddressDistrict;
+
     @Column(name = "open_status")
     private Boolean openStatus;
+
     @Column(name = "rating")
     private int pharmacyRating;
 }
