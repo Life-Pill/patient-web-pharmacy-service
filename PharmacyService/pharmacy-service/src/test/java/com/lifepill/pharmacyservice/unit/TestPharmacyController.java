fc5d8ceeeefcbdbd54b7344b5874aa9da979d430
Inupa Vihan <inupavihan9@gmail.com>
2024-05-11 23:04:43 +0530

    Add unit test for add new pharmacy functionality

--- PharmacyService/pharmacy-service/src/test/java/com/lifepill/pharmacyservice/unit/TestPharmacyController.java
+++ PharmacyService/pharmacy-service/src/test/java/com/lifepill/pharmacyservice/unit/TestPharmacyController.java
@@ -1,12 +1,16 @@
 package com.lifepill.pharmacyservice.unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -52,5 +56,38 @@ public class TestPharmacyController {
         String expected = "{\"pharmacyName\":\"LifePill Pharmacy\",\"pharmacyEmail\":\"lifepill@gmail.com\", \"pharmacyMobileNumber\":\"0771231234\",\"pharmacyAddressStreet\":\"Hospital Road\", \"pharmacyAddressCity\":\"Maharagama\", \"pharmacyAddressDistrict\":\"Colombo\", \"openStatus\": true , \"pharmacyRating\": 5}";
 
         JSONAssert.assertEquals(expected, result.getResponse().getContentAsString(), false);
+    }
+
+    // testing the add new pharmacy functionality
+    @Test
+    public void createNewPharmacy() throws Exception {
+        Pharmacy mockPharmacy = new Pharmacy(
+                1L,
+                "LifePill Pharmacy",
+                "lifepill@gmail.com",
+                "0771231234",
+                "Hospital Road",
+                "Maharagama",
+                "Colombo",
+                true,
+                5,
+                "password");
+
+        Mockito.when(pharmacyService.addPharmacy(Mockito.any(Pharmacy.class))).thenReturn(mockPharmacy);
+
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post("/pharmacies")
+                .accept(MediaType.APPLICATION_JSON).content(examplePharmacyJson)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+
+        MockHttpServletResponse response = result.getResponse();
+
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+        // assertEquals("http://localhost/pharmacies/1",
+        // response.getHeader(HttpHeaders.LOCATION));
+
     }
 }
