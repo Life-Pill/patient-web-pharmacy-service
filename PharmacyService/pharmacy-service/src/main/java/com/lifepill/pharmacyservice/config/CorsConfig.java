810265b8bbf46d9f7c02cbc87f54387282843386
Pramitha Jayasooriya <lpramithamj@gmail.com>
2024-07-12 17:42:45 +0530

    update

--- PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/config/CorsConfig.java
+++ PharmacyService/pharmacy-service/src/main/java/com/lifepill/pharmacyservice/config/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("http://localhost:5173","http://44.218.33.250")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("Authorization", "Cache-Control", "Content-Type")
                         .allowCredentials(true);
